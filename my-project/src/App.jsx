4d8f2b8abbb566bd584655e681ca294cab45f70c
kshitijslays <rajkshitij876@gmail.com>
2025-03-31 17:51:19 +0530

    completed the website

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,8 +1,12 @@
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
-import Benefits from "./components/Benefits"
+import Benefits from "./components/Benefits";
 import Collaboration from "./components/Collaboration";
+import Services from "./components/Services";
+import Pricing from "./components/Pricing";
+import Roadmap from "./components/Roadmap";
+import Footer from "./components/Footer";
 const App = () => {
   return (
     <>
@@ -10,9 +14,13 @@ const App = () => {
         <Header />
         <Hero />
         <Benefits />
+        <Collaboration />
+        <Services />
+        <Pricing />
+        <Roadmap />
+        <Footer />
       </div>
       <ButtonGradient />
-      <Collaboration />
     </>
   );
 };
